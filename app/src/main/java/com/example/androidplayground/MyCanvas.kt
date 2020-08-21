e60c7f6048a2b89bb82cd98ed72fcd2a3b07176f
Rafa <rmoreno.cesar@gmail.com>
2020-08-21 18:58:20 -0500

    Add MyCanvas class
    Played around with the difference between hiding the navigation bar,
    and setting SYSTEM_UI_FLAG_FULLSCREEN flag for the systemUivisibility of the whole view.
    Was neat to see that the flag gets rid of the background where the inset is, that would
    still show up if we had only taken out the navigation bar. :100:

---
+++ app/src/main/java/com/example/androidplayground/MyCanvas.kt
@@ -0,0 +1,14 @@
+package com.example.androidplayground
+
+import android.content.Context
+import android.graphics.Canvas
+import android.view.View
+
+class MyCanvas(context: Context): View(context) {
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+
+    }
+}
