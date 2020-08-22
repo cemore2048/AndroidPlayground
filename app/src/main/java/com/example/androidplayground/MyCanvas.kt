7c9df59b4e93f38e13b05a15d9e43f16f45f9aed
Rafa <rmoreno.cesar@gmail.com>
2020-08-22 15:28:44 -0500

    Don't store path in bitmap, store in Path instead
    Previously we were caching the map in a bitmap and storing an extra canvas
    as well. We switched over to storing the path in a single 'curPath' variable
    and the store the entire drawing, which is an accumulation of the 'curPath' every
    time we move our finger back off the screen.

--- app/src/main/java/com/example/androidplayground/MyCanvas.kt
+++ app/src/main/java/com/example/androidplayground/MyCanvas.kt
@@ -1,7 +1,10 @@
 package com.example.androidplayground
 
 import android.content.Context
-import android.graphics.*
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.Rect
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewConfiguration
@@ -14,12 +17,12 @@ class MyCanvas(context: Context) : View(context) {
         private const val STROKE_WIDTH = 30f
     }
 
-    private lateinit var extraCanvas: Canvas
-    private lateinit var extraBitmap: Bitmap
     private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
     private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
-    private val path = Path()
+
     private lateinit var frame: Rect
+    private val drawing = Path()
+    private val curPath = Path()
 
     // Defines a tolerance that allows us to not draw every single time a users finger moves the tiniest bit
     private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
@@ -45,18 +48,18 @@ class MyCanvas(context: Context) : View(context) {
 
         val inset = 40
         frame = Rect(inset, inset, width - inset, height - inset)
-        // We need to create a new bitmap every time the size of the canvas
-        if (::extraBitmap.isInitialized) extraBitmap.recycle()
 
-        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-        extraCanvas = Canvas(extraBitmap)
-        extraCanvas.drawColor(backgroundColor)
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        canvas.drawBitmap(extraBitmap, 0f, 0f, null)
+        canvas.drawColor(backgroundColor)
+        // Draw the drawing so far
+        canvas.drawPath(drawing, paint)
+        // Draw any current squiggle
+        canvas.drawPath(curPath, paint)
+        // Draw a frame around the canvas
         canvas.drawRect(frame, paint)
     }
 
@@ -77,8 +80,8 @@ class MyCanvas(context: Context) : View(context) {
     private var currentY = 0f
 
     private fun touchStart() {
-        path.reset()
-        path.moveTo(motionTouchEventX, motionTouchEventY)
+        curPath.reset()
+        curPath.moveTo(motionTouchEventX, motionTouchEventY)
         currentX = motionTouchEventX
         currentY = motionTouchEventY
     }
@@ -87,25 +90,23 @@ class MyCanvas(context: Context) : View(context) {
         val dx = abs(motionTouchEventX - currentX)
         val dy = abs(motionTouchEventY - currentY)
 
-            // QuadTo() adds a quadratic bezier from the last point,
-            // approaching control point (x1,y1), and ending at (x2,y2).
-            path.quadTo(
-                currentX,
-                currentY,
-                (motionTouchEventX + currentX) / 2,
-                (motionTouchEventY + currentY) / 2
-            )
-            currentX = motionTouchEventX
-            currentY = motionTouchEventY
-            // Draw the path in the extra bitmap to cache it.
-            extraCanvas.drawPath(path, paint)
-
-
+        // QuadTo() adds a quadratic bezier from the last point,
+        // approaching control point (x1,y1), and ending at (x2,y2).
+        curPath.quadTo(
+            currentX,
+            currentY,
+            (motionTouchEventX + currentX) / 2,
+            (motionTouchEventY + currentY) / 2
+        )
+        currentX = motionTouchEventX
+        currentY = motionTouchEventY
         invalidate()
     }
 
     private fun touchUp() {
-        // Reset the path so it doesn't get drawn again.
-        path.reset()
+        // Add the current path to the drawing so far
+        drawing.addPath(curPath)
+        // Rewind the current path for the next touch
+        curPath.reset()
     }
 }
