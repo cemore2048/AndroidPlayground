f0aec7557a0587f11e2b725b32ff950159be7a60
Rafa <rmoreno.cesar@gmail.com>
2020-08-21 19:58:02 -0500

    Set up paint and path for drawing

--- app/src/main/java/com/example/androidplayground/MyCanvas.kt
+++ app/src/main/java/com/example/androidplayground/MyCanvas.kt
@@ -3,15 +3,36 @@ package com.example.androidplayground
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
 
 class MyCanvas(context: Context): View(context) {
 
+    companion object {
+        private const val STROKE_WIDTH = 12f
+
+    }
+
     private lateinit var extraCanvas: Canvas
     private lateinit var extraBitmap: Bitmap
     private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+    private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
+    private val path = Path()
 
+    private val paint = Paint().apply {
+        color = drawColor
+        // Smooth out edges of what is drawn without affecting shape
+        isAntiAlias = true
+        // Dithering affects how colors with higher precision than the device are down-sampled
+        isDither = true
+        style = Paint.Style.STROKE
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
+        strokeWidth = STROKE_WIDTH
+
+    }
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
 
