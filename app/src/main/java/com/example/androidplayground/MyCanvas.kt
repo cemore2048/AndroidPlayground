8364a4829237d105708489fc69a2033db2d92edb
Rafa <rmoreno.cesar@gmail.com>
2020-08-22 12:21:10 -0500

    Draw paint when moving, and include frame when app loads

--- app/src/main/java/com/example/androidplayground/MyCanvas.kt
+++ app/src/main/java/com/example/androidplayground/MyCanvas.kt
@@ -20,22 +20,32 @@ class MyCanvas(context: Context): View(context) {
     private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
     private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
     private val path = Path()
+    private lateinit var frame: Rect
+
+    // Defines a tolerance that allows us to not draw every single time a users finger moves the tiniest bit
+    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
+
+    private var motionTouchEventX = 0f
+    private var motionTouchEventY = 0f
 
     private val paint = Paint().apply {
         color = drawColor
         // Smooth out edges of what is drawn without affecting shape
         isAntiAlias = true
         // Dithering affects how colors with higher precision than the device are down-sampled
-        isDither = true
+        isDither = false
         style = Paint.Style.STROKE
         strokeJoin = Paint.Join.ROUND
         strokeCap = Paint.Cap.ROUND
         strokeWidth = STROKE_WIDTH
 
     }
+
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
 
+        val inset = 40
+        frame = Rect(inset, inset, width - inset, height - inset)
         // We need to create a new bitmap every time the size of the canvas
         if (::extraBitmap.isInitialized) extraBitmap.recycle()
 
@@ -43,10 +53,56 @@ class MyCanvas(context: Context): View(context) {
         extraCanvas = Canvas(extraBitmap)
         extraCanvas.drawColor(backgroundColor)
     }
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
         canvas.drawBitmap(extraBitmap, 0f, 0f, null)
+        canvas.drawRect(frame, paint)
+    }
 
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        motionTouchEventX = event.x
+        motionTouchEventY = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> touchStart()
+            MotionEvent.ACTION_MOVE -> touchMove()
+            MotionEvent.ACTION_UP -> touchUp()
+        }
+
+        return true
+    }
+
+    private var currentX = 0f
+    private var currentY = 0f
+
+    private fun touchStart() {
+        path.reset()
+        path.moveTo(motionTouchEventX, motionTouchEventY)
+        currentX = motionTouchEventX
+        currentY = motionTouchEventY
+    }
+
+    private fun touchMove() {
+        val dx = abs(motionTouchEventX - currentX)
+        val dy = abs(motionTouchEventY - currentY)
+
+        if (dx >= touchTolerance || dy >= touchTolerance) {
+            // QuadTo() adds a quadratic bezier from the last point,
+            // approaching control point (x1,y1), and ending at (x2,y2).
+            path.quadTo(currentX, currentY, (motionTouchEventX + currentX) / 2, (motionTouchEventY + currentY) / 2)
+            currentX = motionTouchEventX
+            currentY = motionTouchEventY
+            // Draw the path in the extra bitmap to cache it.
+            extraCanvas.drawPath(path, paint)
+        }
+
+        invalidate()
+    }
+
+    private fun touchUp() {
+        // Reset the path so it doesn't get drawn again.
+        path.reset()
     }
 }
