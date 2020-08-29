fa62368352f859654c6bfcb049069d831e54a7c4
Rafa <rmoreno.cesar@gmail.com>
2020-08-28 19:50:18 -0500

    Import Region property

--- app/src/main/java/com/example/androidplayground/ClippedView.kt
+++ app/src/main/java/com/example/androidplayground/ClippedView.kt
@@ -1,10 +1,7 @@
 package com.example.androidplayground
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.os.Build
 import android.util.AttributeSet
 import android.view.View
