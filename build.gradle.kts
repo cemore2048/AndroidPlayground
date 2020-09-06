ffae910c502a8aff72d081480067c7466124aec5
Rafa <rmoreno.cesar@gmail.com>
2020-09-06 18:12:29 -0500

    Convert root build.gradle file to kotlin dsl

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.kotlin_version = '1.4.0'
+    val kotlinVersion by rootProject.extra { "1.4.0" }
     repositories {
         google()
         jcenter()
         maven {
-            url = "https://dosh.jfrog.io/dosh/android"
+            url = uri("https://dosh.jfrog.io/dosh/android")
             credentials {
                 username = ""
                 password = ""
@@ -15,8 +15,8 @@ buildscript {
 
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:4.0.1'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:4.0.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${kotlinVersion}")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -31,6 +31,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
