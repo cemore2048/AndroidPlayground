8b427b7df8be163cf06f9262fdd5c70e860d03a4
Rafa <rmoreno.cesar@gmail.com>
2020-09-16 22:16:19 -0500

    Update build.gradle to include correct dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    val kotlinVersion by rootProject.extra { "1.4.0" }
+    val kotlinVersion by rootProject.extra { "1.4.10" }
     repositories {
         google()
         jcenter()
