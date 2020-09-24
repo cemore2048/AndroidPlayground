3460b4070f895f637603d67e91575bae379a48d4
Rafa <rmoreno.cesar@gmail.com>
2020-09-23 22:47:55 -0500

    Added separate tooling for previews

--- app/src/main/java/com/example/androidplayground/MainActivity.kt
+++ app/src/main/java/com/example/androidplayground/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.androidplayground
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import androidx.compose.foundation.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +15,21 @@ class MainActivity : AppCompatActivity() {
 //        myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
 //        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
 
+        setContent {
+            Greeting("Hello, World")
+        }
         val clippedView = ClippedView(this)
         setContentView(clippedView)
+    }
+
+    @Composable
+    fun Greeting(name: String) {
+        Text(name)
+    }
+
+    @Preview
+    @Composable
+    fun PreviewGreeting() {
+        Greeting("This is my greeting")
     }
 }
