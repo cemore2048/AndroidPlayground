9afc9784d57fc40e63f550b22ea0c6f095f3e1e4
Rafa Moreno Cesar <rafa.moreno-cesar@instacart.com>
2021-05-16 10:39:58 -0500

    Load image natively

--- app/src/main/java/com/example/androidplayground/MainActivity.kt
+++ app/src/main/java/com/example/androidplayground/MainActivity.kt
@@ -1,74 +1,33 @@
 package com.example.androidplayground
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.Text
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.MaterialTheme.typography
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.res.imageResource
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.dp
-import androidx.ui.tooling.preview.Preview
+import android.os.Handler
+import android.os.Looper
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import java.io.InputStream
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+    
+    private lateinit var catImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val myCanvasView = MyCanvas(this)
-//        myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
-//        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
-        Person("Big Bird", "123 Seasame Street")
-//        setContent {
-//            Greeting("Hello, World")
-//        }
-        val clippedView = ClippedView(this)
-        setContentView(clippedView)
-    }
+        setContentView(R.layout.activity_main)
 
-//    @Composable
-//    fun Greeting(name: String) {
-//        Text(name)
-//    }
-//
-//    @Preview
-//    @Composable
-//    fun PreviewGreeting() {
-//        Greeting("This is my greeting")
-//    }
-//
-//    @Composable
-//    fun NewsStory() {
-//        val image = imageResource(R.drawable.ic_launcher_background)
-//        MaterialTheme {
-//            Column(modifier = Modifier.padding(16.dp)) {
-//                val imageModifier = Modifier
-//                    .preferredHeight(180.dp)
-//                    .fillMaxWidth()
-//                    .clip(shape = RoundedCornerShape(4.dp))
-//                Image(image, modifier = imageModifier, contentScale = ContentScale.Crop)
-//                Spacer(Modifier.preferredHeight(16.dp))
-//                Text( "A day wandering through the sandhills " +
-//                        "in Shark Fin Cove, and a few of the " +
-//                        "sights I saw",
-//                style = typography.h6,
-//                maxLines = 2,
-//                overflow = TextOverflow.Ellipsis)
-//                Text("Davenport, California")
-//                Text("December 2018")
-//            }
-//        }
-//    }
-//    @Preview
-//    @Composable
-//    fun DefaultPreview() {
-//        NewsStory()
-//    }
+        catImage = findViewById(R.id.image)
+
+        Thread(object : Runnable {
+            var bitmap: Bitmap? = null
+            override fun run() {
+                val input: InputStream = URL("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.RGO2_JeImrSM658MPIASrwHaEo%26pid%3DApi&f=1").openStream()
+                bitmap = BitmapFactory.decodeStream(input)
+
+                Handler(Looper.getMainLooper()).post { catImage.setImageBitmap(bitmap) }
+            }
+        }).start()
+    }
 }
