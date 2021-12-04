14005a3dbe32f985106b3d7d634e24cf4717b809
Rafa Moreno Cesar <rafa.moreno-cesar@instacart.com>
2021-12-04 13:58:26 -0600

    Add response type in execution repository

--- app/src/main/java/com/example/androidplayground/ExecutionRepository.kt
+++ app/src/main/java/com/example/androidplayground/ExecutionRepository.kt
@@ -1,4 +1,9 @@
 package com.example.androidplayground
 
-class ExecutionRepository(executionEndpoint: ExecutionEndpoint) {
+import io.reactivex.Observable
+
+class ExecutionRepository(private val executionEndpoint: ExecutionEndpoint) {
+    fun execute(language: String, command: String): Observable<ExecutionResult> {
+        return executionEndpoint.execute(language, command)
+    }
 }
