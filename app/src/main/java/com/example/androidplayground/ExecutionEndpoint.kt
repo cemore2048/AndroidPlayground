2ec6b93ed85c1a4d1632d9fc98af502fb0975916
Rafa Moreno Cesar <rafa.moreno-cesar@instacart.com>
2021-12-28 08:56:41 -0700

    Clean up execution endpoint with adapters

--- app/src/main/java/com/example/androidplayground/ExecutionEndpoint.kt
+++ app/src/main/java/com/example/androidplayground/ExecutionEndpoint.kt
@@ -1,12 +1,17 @@
 package com.example.androidplayground
 
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
 interface ExecutionEndpoint {
 
-   @POST
 }
 
 private val retrofit: Retrofit = Retrofit.Builder()
    .baseUrl("")
+   .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+   .addConverterFactory(GsonConverterFactory.create())
    .build()
 
-val endpoint = retrofit.create(ExecutionEndpoint::class)
+val endpoint = retrofit.create(ExecutionEndpoint::class.java)
--- app/src/main/java/com/example/androidplayground/ExecutionRepository.kt
+++
@@ -1,4 +0,0 @@
-package com.example.androidplayground
-
-class ExecutionRepository(executionEndpoint: ExecutionEndpoint) {
-}
